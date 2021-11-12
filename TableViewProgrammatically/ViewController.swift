11ed43a67df3bc63d0ec2aef96e815501407810f
Tambanco <52546246+Tambanco@users.noreply.github.com>
2021-11-12 19:46:32 +0300

    added tableView into view with constraints

--- TableViewProgrammatically/ViewController.swift
+++ TableViewProgrammatically/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initializeTableView()
+        
     }
-
+    
+    private func initializeTableView() {
+        tableView = UITableView()
+        tableView.rowHeight = 200
+        tableView.backgroundColor = .blue
+        
+        self.view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50).isActive = true
+        tableView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive = true
+        tableView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1).isActive = true
+    }
 
 }
 

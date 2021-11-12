b17fb013bd5ec786fa32fda59ce7fe2ed3625aa6
Tambanco <52546246+Tambanco@users.noreply.github.com>
2021-11-12 22:54:53 +0300

    added InstagramCell.swift

---
+++ TableViewProgrammatically/InstagramCell.swift
@@ -0,0 +1,12 @@
+//
+//  InstagramCell.swift
+//  TableViewProgrammatically
+//
+//  Created by tambanco 🥳 on 12.11.2021.
+//
+
+import UIKit
+class InstagramCell: UITableViewCell {
+    
+    static var reuseID = "InstagramCell"
+}

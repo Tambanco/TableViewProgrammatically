0208249e2473334fb0ebf91ddf0b781bef55a40e
Tambanco <52546246+Tambanco@users.noreply.github.com>
2021-11-12 19:29:05 +0300

    Initial Commit

---
+++ TableViewProgrammatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewProgrammatically
+//
+//  Created by tambanco 🥳 on 12.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
